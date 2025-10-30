44ab6f829a8b898c90685015c7aa73829aeb2d38
hyeongdong <khd941018@gmail.com>
2025-10-30 23:17:06 +0900

    test(Lotto): contains 메서드 테스트 코드 작성
    보너스 번호와 일치하는 숫자가 있는 경우 TRUE를 반환, 없는 경우 FALSE를 반환
    하기 때문에 TRUE를 반환하는 경우와 FALSE 반환하는 경우 모두 테스트하도록
    두 경우의 테스트 코드를 모두 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -25,4 +25,18 @@ describe("로또 클래스 테스트", () => {
     const lotto = new Lotto(numbers);
     expect(lotto.countMatches(winningNumbers)).toBe(matchCount);
   });
+
+  test.each([[[1, 2, 3, 4, 5, 6]], [[1, 3, 4, 7, 8, 9]], [[4, 10, 12, 13, 14, 15]]])("로또 번호 중에 보너스 번호와 일치하는 숫자가 있다면 true를 반환한다.", (numbers) => {
+    const bonusNumber = 4;
+    const lotto = new Lotto(numbers);
+
+    expect(lotto.contains(bonusNumber)).toBeTruthy();
+  });
+
+  test.each([[[1, 2, 3, 4, 5, 6]], [[1, 3, 4, 7, 8, 9]], [[4, 10, 12, 13, 14, 15]]])("로또 번호 중에 보너스 번호와 일치하는 숫자가 없다면 false를 반환한다.", (numbers) => {
+    const bonusNumber = 30;
+    const lotto = new Lotto(numbers);
+
+    expect(lotto.contains(bonusNumber)).toBeFalsy();
+  });
 });
