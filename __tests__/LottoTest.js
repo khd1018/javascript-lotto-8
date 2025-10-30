472150982eb450716013f1d934081979a37cabcf
hyeongdong <khd941018@gmail.com>
2025-10-30 22:55:04 +0900

    test(Lotto): countMatches 메서드 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,14 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([
+    [[1, 2, 3, 4, 5, 6], 6],
+    [[1, 3, 4, 7, 8, 9], 3],
+    [[8, 10, 12, 13, 14, 15], 0],
+  ])("로또 번호 중에 당첨 번호와 일치하는 숫자가 몇 개 있는지 알 수 있다.", (numbers, matchCount) => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+
+    const lotto = new Lotto(numbers);
+    expect(lotto.countMatches(winningNumbers)).toBe(matchCount);
+  });
 });
