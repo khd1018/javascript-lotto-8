bc1ee59143455a8d9746988de5b87d4db8262b82
hyeongdong <khd941018@gmail.com>
2025-11-03 00:44:12 +0900

    feat(OutputView): 당첨 내역을 출력하는 기능 구현

---
+++ src/views/OutputView.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export const OutputView = {
+  RANK_STRING: {
+    first: "6개 일치",
+    second: "5개 일치, 보너스 볼 일치",
+    third: "5개 일치 (1,500,000원)",
+    fourth: "4개 일치 (50,000원)",
+    fifth: "3개 일치 (5,000원)",
+  },
+
+  printNumbers(lottos) {
+    Console.print(`${lottos.length}개를 구매했습니다.\n`);
+
+    lottos.forEach((lotto) => {
+      Console.print(lotto);
+    });
+  },
+
+  printRankStats(rankStats) {
+    Console.print("당첨 통계\n---");
+
+    Object.keys(this.RANK_STRING).forEach((rank) => {
+      Console.print(`${this.RANK_STRING[rank]} - ${rankStats[rank]}`);
+    });
+  },
+};
