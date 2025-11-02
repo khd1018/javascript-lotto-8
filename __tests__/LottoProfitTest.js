1785879b1b24779609bec8ccc6d1edd1f7c5cecc
hyeongdong <khd941018@gmail.com>
2025-11-03 02:50:18 +0900

    test(LottoProfit): 클래스 변경으로 인한 테스트 코드 수정

--- __tests__/LottoProfitTest.js
+++ __tests__/LottoProfitTest.js
@@ -7,18 +7,10 @@ describe("LottoProfit 테스트", () => {
     [[], "0.0"],
   ])("구매한 로또의 총 수익률을 구할 수 있다.", (ranks, totalProfitRate) => {
     const budget = 8000;
-    const lottoProfit = new LottoProfit(budget);
+    const lottoProfit = new LottoProfit();
 
-    ranks.forEach((rank) => lottoProfit.sumAllProfits(rank));
+    ranks.forEach((rank) => lottoProfit.addPrizeBy(rank));
 
-    expect(lottoProfit.calculateProfitRate()).toBe(totalProfitRate);
-  });
-
-  test.each([[1], [300], [1001]])("구입금액이 1,000원 단위가 아니면 예외가 발생한다. ", (budget) => {
-    expect(() => new LottoProfit(budget)).toThrow("[ERROR]");
-  });
-
-  test.each([[100001], [200000]])("구입금액이 100,000원을 초과하면 예외가 발생한다. ", (budget) => {
-    expect(() => new LottoProfit(budget)).toThrow("[ERROR]");
+    expect(lottoProfit.calculateProfitRate(budget)).toBe(totalProfitRate);
   });
 });
