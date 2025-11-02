3478df270c09eb34ef770b6192679bb36914da6e
hyeongdong <khd941018@gmail.com>
2025-11-02 23:54:46 +0900

    test(LottoEvaluator): 당첨번호와 보너스번호 검증 기능 테스트 작성

--- __tests__/LottoEvaluatorTest.js
+++ __tests__/LottoEvaluatorTest.js
@@ -1,5 +1,5 @@
-import Lotto from "../src/Lotto";
-import LottoEvaluator from "../src/LottoEvaluator";
+import Lotto from "../src/models/Lotto";
+import LottoEvaluator from "../src/models/LottoEvaluator";
 
 describe("LottoEvaluator 테스트", () => {
   test.each([
@@ -12,5 +12,37 @@ describe("LottoEvaluator 테스트", () => {
     const lottoEvaluator = new LottoEvaluator(bonusNumber, winningNumbers);
 
     expect(lottoEvaluator.rateRank(new Lotto(numbers))).toBe(rank);
+  });
+
+  test.each([
+    [0, [1, 2, 3, 4, 5, 6]],
+    [46, [1, 2, 3, 4, 5, 6]],
+    [25, [1, 2, 3, 4, 5, 60]],
+  ])("숫자가 1~45사이가 아닌 경우 예외가 발생한다. ", (bonusNumber, winningNumbers) => {
+    expect(() => new LottoEvaluator(bonusNumber, winningNumbers)).toThrow("[ERROR]");
+  });
+
+  test.each([
+    [1, [1, 2, 3, 4, 5, 6, 7]],
+    [45, [1, 2, 3, 4, 5]],
+    [25, [1]],
+  ])("당첨번호가 6개가 아닌 경우 예외가 발생한다. ", (bonusNumber, winningNumbers) => {
+    expect(() => new LottoEvaluator(bonusNumber, winningNumbers)).toThrow("[ERROR]");
+  });
+
+  test.each([
+    [1, [1, 2, 3, 4, 5, 6]],
+    [45, [1, 2, 3, 4, 45]],
+    [25, [1, 2, 3, 4, 5, 25]],
+  ])("당첨번호에 보너스번호와 같은 숫자가 있으면 예외가 발생한다. ", (bonusNumber, winningNumbers) => {
+    expect(() => new LottoEvaluator(bonusNumber, winningNumbers)).toThrow("[ERROR]");
+  });
+
+  test.each([
+    [1, [1, 2, 3, 4, 6, 6]],
+    [45, [1, 2, 3, 4, 4, 5]],
+    [25, [1, 2, 3, 4, 5, 5]],
+  ])("당첨번호에 중복된 숫자가 있으면 예외가 발생한다. ", (bonusNumber, winningNumbers) => {
+    expect(() => new LottoEvaluator(bonusNumber, winningNumbers)).toThrow("[ERROR]");
   });
 });
