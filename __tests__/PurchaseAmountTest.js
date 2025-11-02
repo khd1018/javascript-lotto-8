2209aa31c8d73dbc7a8eeed748326378f4aa9ce2
hyeongdong <khd941018@gmail.com>
2025-11-03 02:53:34 +0900

    test(PurchaseAmount): 구매할 수 있는 로또 개수를 구하는 기능 테스트 작성

--- __tests__/PurchaseAmountTest.js
+++ __tests__/PurchaseAmountTest.js
@@ -8,4 +8,13 @@ describe("LottoProfit 테스트", () => {
   test.each([[100001], [200000]])("구입금액이 100,000원을 초과하면 예외가 발생한다. ", (budget) => {
     expect(() => new PurchaseAmount(budget)).toThrow("[ERROR]");
   });
+
+  test.each([
+    [3000, 3],
+    [5000, 5],
+  ])("구매할 수 있는 로또의 개수를 구할 수 있다.", (budget, lottoCount) => {
+    const purchaseAmount = new PurchaseAmount(budget);
+
+    expect(purchaseAmount.getLottoCount()).toBe(lottoCount);
+  });
 });
