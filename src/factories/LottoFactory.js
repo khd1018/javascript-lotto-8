e5e3932f2ebf1892751f8c72b09f632af1efe659
hyeongdong <khd941018@gmail.com>
2025-11-03 03:05:55 +0900

    fix(LottoFactory): Lotto 경로 수정

--- src/factories/LottoFactory.js
+++ src/factories/LottoFactory.js
@@ -1,5 +1,5 @@
 import { Random } from "@woowacourse/mission-utils";
-import Lotto from "./models/Lotto.js";
+import Lotto from "../models/Lotto.js";
 
 class LottoFactory {
   static createLottos(lottoCount) {
