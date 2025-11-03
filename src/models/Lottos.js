c6ee4e482d799749138a6d03c749794199017b51
hyeongdong <khd941018@gmail.com>
2025-11-03 15:52:44 +0900

    feat(Lottos): 로또들의 등급을 찾는 기능 구현
    기존에는 controller에서 lotto들에 대한 결과를 구할 때마다 매번 배열 순회
    메서드를 사용해야 했는데 이를 전체 흐름을 담당하는 controller에서 담당하는
    것보다 lotto객체들을 관리하는 Lottos를 추가해 여기서 순회를 하면서 결과를
    구하는 것이 적절하다고 생각해 Lottos를 구현
    
    또 등급을 찾는 기능은 LottoEvaluator에서 담당하는 것보다 Lottos에서 담당하는게
    적절하다고 생각해 해당 기능을 Lottos에서 구현(기존 LottoEvaluator는 model보다는
    util의 성격이 강하였고 bonusNumber, winningNumbers를 상태로 관리할 필요가 없었고
    기존방식대로라면 controller에서 배열을 순회했어야 했음)

---
+++ src/models/Lottos.js
@@ -0,0 +1,31 @@
+class Lottos {
+  #lottos;
+
+  constructor(lottos) {
+    this.#lottos = lottos;
+  }
+
+  getLottoRanks(bonusNumber, winningNumbers) {
+    const RANK_MAP = {
+      3: "fifth",
+      4: "fourth",
+      5: "third",
+      6: "first",
+    };
+
+    return this.#lottos.map((lotto) => {
+      const matchCount = lotto.countMatches(winningNumbers);
+      const rank = RANK_MAP[matchCount];
+
+      if (matchCount === 5 && lotto.contains(bonusNumber)) {
+        return "second";
+      }
+
+      if (rank) {
+        return rank;
+      }
+    });
+  }
+}
+
+export default Lottos;
