a6bc74dd140d51b839bf1a81cef49dad10941552
hyeongdong <khd941018@gmail.com>
2025-11-03 20:38:16 +0900

    refactor(validators): 에러 메세지 상수 분리

--- src/validators/InputValidator.js
+++ src/validators/InputValidator.js
@@ -1,13 +1,15 @@
+import { ERROR_MESSAGE } from "../constants/messages";
+
 class InputValidator {
   static checkEmpty(userInput) {
     if (!userInput) {
-      throw new Error("[ERROR]값을 입력해주세요.");
+      throw new Error(ERROR_MESSAGE.EMPTY_INPUT);
     }
   }
 
   static checkNumber(userInput) {
     if (!Number.isInteger(Number(userInput))) {
-      throw new Error("[ERROR]숫자를 입력해주세요.");
+      throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
     }
   }
 }
--- src/validators/LottoValidator.js
+++ src/validators/LottoValidator.js
@@ -1,25 +1,27 @@
+import { ERROR_MESSAGE } from "../constants/messages";
+
 class LottoValidator {
   static validateOutOfRange(number) {
     if (number < 1 || number > 45) {
-      throw new Error("[ERROR] 1~45사이의 숫자를 입력하셔야 합니다.");
+      throw new Error(ERROR_MESSAGE.OUT_OF_RANGE);
     }
   }
 
   static validateCount(winningNumbers) {
     if (winningNumbers.length !== 6) {
-      throw new Error("[ERROR] 당첨 번호는 숫자 6개만 입력하셔야 합니다.");
+      throw new Error(ERROR_MESSAGE.INVALID_COUNTS);
     }
   }
 
   static validateBonusDuplication(bonusNumber, winningNumbers) {
     if (winningNumbers.includes(bonusNumber)) {
-      throw new Error("[ERROR] 보너스번호는 당첨번호와 중복되지 않는 숫자를 입력하셔야 합니다.");
+      throw new Error(ERROR_MESSAGE.DUPLICATE_BONUS);
     }
   }
 
   static validateDuplication(winningNumbers) {
     if (new Set(winningNumbers).size < winningNumbers.length) {
-      throw new Error("[ERROR] 당첨 번호는 서로 다른 숫자들을 입력하셔야 합니다.");
+      throw new Error(ERROR_MESSAGE.DUPLICATE_NUMBERS);
     }
   }
 }
