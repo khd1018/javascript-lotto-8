e330f148cf974968c0640c4ca3f7a1dd4ee4b4c4
hyeongdong <khd941018@gmail.com>
2025-11-03 17:37:04 +0900

    fix(InputValidator): 잘못된 에러메세지 수정

--- src/validators/InputValidator.js
+++ src/validators/InputValidator.js
@@ -1,13 +1,13 @@
 class InputValidator {
   static checkEmpty(userInput) {
     if (!userInput) {
-      throw new Error("값을 입력해주세요.");
+      throw new Error("[ERROR]값을 입력해주세요.");
     }
   }
 
   static checkNumber(userInput) {
     if (!Number.isInteger(Number(userInput))) {
-      throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
+      throw new Error("[ERROR]숫자를 입력해주세요.");
     }
   }
 }
