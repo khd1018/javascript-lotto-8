0547fd58e585133e4b6fb9d430a763edc60981fd
hyeongdong <khd941018@gmail.com>
2025-11-03 20:11:22 +0900

    fix(OutputView): 출력 형식에 맞게 출력되도록 수정

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -2,11 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 
 export const OutputView = {
   RANK_STRING: {
-    first: "6개 일치",
-    second: "5개 일치, 보너스 볼 일치",
-    third: "5개 일치 (1,500,000원)",
-    fourth: "4개 일치 (50,000원)",
     fifth: "3개 일치 (5,000원)",
+    fourth: "4개 일치 (50,000원)",
+    third: "5개 일치 (1,500,000원)",
+    second: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+    first: "6개 일치 (2,000,000,000원)",
   },
 
   print(message) {
@@ -14,10 +14,10 @@ export const OutputView = {
   },
 
   printNumbers(lottos) {
-    Console.print(`${lottos.length}개를 구매했습니다.\n`);
+    Console.print(`${lottos.length}개를 구매했습니다.`);
 
     lottos.forEach((lotto) => {
-      Console.print(lotto);
+      Console.print(`[${lotto.join(", ")}]`);
     });
   },
 
@@ -25,7 +25,7 @@ export const OutputView = {
     Console.print("당첨 통계\n---");
 
     Object.keys(this.RANK_STRING).forEach((rank) => {
-      Console.print(`${this.RANK_STRING[rank]} - ${rankStats[rank]}개\n`);
+      Console.print(`${this.RANK_STRING[rank]} - ${rankStats[rank]}개`);
     });
   },
 
